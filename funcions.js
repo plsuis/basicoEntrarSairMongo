c305b3b943b785b6be55a88c8572c3270d237079
suplis <israel.surribas.planas@gmail.com>
2024-01-18 14:05:20 +0100

    Modifico varios arquivos no CLIENTE
    Modifico o index.html:
    
    > Modifico o formulario, quitando os atributos de 'action' e 'method', e paso a poñer dous atributos na etiqueta form e no input de submit
    
    > Os atributos son:
    
     ->'datosForm' na etiqueta form, para recoller os datos a través de 'new FormData'
    
     -> 'envioForm' do input para enviar
    
    Modifico o index.js:
    
    > Agrego un evento click, a función asociada o evento será asíncrono, dito evento ten un fetch que ten un endpoint e os datos para enviar, mediante o body e method
    
    Nel, tamén recollemos a resposta mediante 'await envio.json()' do enviado polo back

--- funcions.js
+++ funcions.js
@@ -1,10 +1,12 @@
 const {isUser} = require("./funcions/lerUser.js")
 
 const leoUsers = async (req,res,next) =>{
-    
     const {user,pwd} = req.body  
     let rexistrado = await isUser(user,pwd)
-    rexistrado ? next() : res.send("O usuario ou pwd non son correctos")
+    let paxina = `<h1>hola</h1>`;
+    rexistrado ? next() : res.send(paxina)
+
+    //rexistrado ? next() : res.send("O usuario ou pwd non son correctos")
 }
 
 module.exports= {
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,12 @@
+envioForm.addEventListener("click",async (e)=>{
+    let datoEnvio = {
+        method:'POST',
+        body: new FormData(datosForm)
+    }
+    let envio = await fetch('/logueo',datoEnvio)
+    let resposta = await envio.json()
 
-
-
-
+    /**
+     * if resposta = logueado =>
+     */
+})

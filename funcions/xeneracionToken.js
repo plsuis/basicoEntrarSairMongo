baadbe8e203c867215190a470f8f81c3580c21c7
suplis <israel.surribas.planas@gmail.com>
2024-01-22 11:19:45 +0100

    Creación do arquivo do Token
    Este arquivo vai crealo token que se envia o cliente

--- funcions/xeneracionToken.js
+++ funcions/xeneracionToken.js
@@ -3,7 +3,7 @@ let jwt = require('jsonwebtoken');
 const enviarToken = (req,res)=>{
     let conxunto = req.body.nome + req.body.pwd
     console.log('o token: ',req.body,conxunto,req.body.nome)
-    let token1 = jwt.sign({ dato: conxunto }, 'shhhhh');
+    let token1 = jwt.sign({ dato: conxunto }, process.env.SECRETO);
 
     res.send({
         mensaxe:token1
